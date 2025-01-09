d09aa6a6f1172336252eee54457a626f3622ff63
ElliotScher <ecscher84@gmail.com>
2025-01-09 02:17:40 -0500

    Update choreo and test

--- src/main/java/frc/robot/commands/AutonomousCommands.java
+++ src/main/java/frc/robot/commands/AutonomousCommands.java
@@ -1,3 +1,23 @@
 package frc.robot.commands;
 
-public class AutonomousCommands {}
+import choreo.Choreo;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.RobotState;
+import frc.robot.subsystems.shared.drive.Drive;
+
+public class AutonomousCommands {
+  public static final Command blueBarge5PieceAuto(Drive drive) {
+    return Commands.sequence(
+        Commands.runOnce(
+            () ->
+                RobotState.resetRobotPose(
+                    Choreo.loadTrajectory("BB1").get().getInitialPose(true).get())),
+        drive.getAutoFactory().trajectoryCmd("BB1"),
+        drive.getAutoFactory().trajectoryCmd("BB2"),
+        drive.getAutoFactory().trajectoryCmd("BB3"),
+        drive.getAutoFactory().trajectoryCmd("BB4"),
+        drive.getAutoFactory().trajectoryCmd("BB5"),
+        drive.getAutoFactory().trajectoryCmd("BB6"));
+  }
+}
