a9302095daab15ed5a7989d534314201a3ad8fb7
Anshu Adiga <aadiga@wpi.edu>
2025-01-14 23:21:37 -0500

    add sysId characterization command

--- src/main/java/frc/robot/subsystems/v1_gamma/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/v1_gamma/elevator/Elevator.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems.v1_gamma.elevator;
 
+import static edu.wpi.first.units.Units.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.subsystems.v1_gamma.elevator.ElevatorConstants.ElevatorPositions;
 import org.littletonrobotics.junction.Logger;
 
@@ -9,12 +13,24 @@ public class Elevator extends SubsystemBase {
   private final ElevatorIO io;
   private final ElevatorIOInputsAutoLogged inputs;
   private ElevatorConstants.ElevatorPositions position;
+  private boolean isClosedLoop;
+  private final SysIdRoutine sysIdRoutine;
 
   public Elevator(ElevatorIO io) {
     this.io = io;
     inputs = new ElevatorIOInputsAutoLogged();
 
     position = ElevatorPositions.STOW;
+
+    isClosedLoop = true;
+    sysIdRoutine =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                Volts.of(0.2).per(Second),
+                Volts.of(3.5),
+                Seconds.of(10.0),
+                (state) -> Logger.recordOutput("Elevator", state.toString())),
+            new SysIdRoutine.Mechanism((volts) -> io.setCurrent(volts.in(Volts)), null, this));
   }
 
   @Override
@@ -23,11 +39,17 @@ public class Elevator extends SubsystemBase {
     Logger.processInputs("Elevator", inputs);
 
     Logger.recordOutput("Elevator/Position", position.name());
-    io.setPositionGoal(position.getPosition());
+
+    if (isClosedLoop) {
+      io.setPositionGoal(position.getPosition());
+    }
   }
 
   public Command setPosition(ElevatorConstants.ElevatorPositions position) {
-    return runOnce(() -> this.position = position);
+    return runOnce(() -> {
+      isClosedLoop = true;
+      this.position = position;
+    });
   }
 
   public Command resetPosition() {
@@ -35,6 +57,19 @@ public class Elevator extends SubsystemBase {
         .andThen(
             runOnce(
                 () -> io.setPosition(ElevatorConstants.ELEVATOR_SIM_PARAMS.MIN_HEIGHT_METERS())));
+  }
+
+  public Command runSysId() {
+    return Commands.sequence(
+      runOnce(()->isClosedLoop = false),
+      sysIdRoutine.quasistatic(Direction.kForward),
+      Commands.waitSeconds(2),
+      sysIdRoutine.quasistatic(Direction.kReverse),
+      Commands.waitSeconds(2),
+      sysIdRoutine.dynamic(Direction.kForward),
+      Commands.waitSeconds(2),
+      sysIdRoutine.dynamic(Direction.kReverse)
+    );
   }
 
   public ElevatorConstants.ElevatorPositions getPosition() {
