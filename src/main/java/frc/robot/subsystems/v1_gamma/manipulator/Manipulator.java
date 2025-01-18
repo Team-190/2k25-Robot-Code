145b42e8b33b5922659f73e39981db4b154449dc
vaddadisri26 <vaddadi.sriaditya@gmail.com>
2025-01-18 13:41:28 -0500

    Created Manipulator Subsystem.

---
+++ src/main/java/frc/robot/subsystems/v1_gamma/manipulator/Manipulator.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.v1_gamma.manipulator;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.Logger;
+
+public class Manipulator extends SubsystemBase {
+  private final ManipulatorIO io;
+  private final ManipulatorIOInputsAutoLogged inputs;
+
+  public Manipulator(ManipulatorIO io) {
+    this.io = io;
+    inputs = new ManipulatorIOInputsAutoLogged();
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Manipulators", inputs);
+  }
+
+  public Command runManipulator(DoubleSupplier forward, DoubleSupplier reverse) {
+    return Commands.run(
+        () -> io.setVoltage(12 * (forward.getAsDouble() - reverse.getAsDouble())), this);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/v1_gamma/manipulator/ManipulatorConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.v1_gamma.manipulator;
+
+public class ManipulatorConstants {
+  public static final int MANIPULATOR_CAN_ID = 1;
+  public static final double SupplyCurrentLimit = 40;
+}
