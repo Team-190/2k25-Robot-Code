e03fbe36ad91273e80406de29d592c6655917146
Anshu Adiga <aadiga@wpi.edu>
2025-02-03 17:29:55 -0500

    Add inchMovement command for precise drive control

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -148,6 +148,33 @@ public final class DriveCommands {
         drive);
   }
 
+  public static final Command inchMovement(Drive drive, double x) {
+    final Double[] distanceBefore = {0.0};
+    return Commands.run(
+            () -> {
+              if (distanceBefore[0] >= drive.getModulePositions()[0].distanceMeters + x) {
+                drive.stop();
+                return;
+              }
+              drive.runVelocity(
+                  ChassisSpeeds.fromFieldRelativeSpeeds(
+                      5.0,
+                      0.0,
+                      0.0,
+                      DriverStation.getAlliance().isPresent()
+                              && DriverStation.getAlliance().get() == Alliance.Red
+                          ? RobotState.getRobotPoseField()
+                              .getRotation()
+                              .plus(new Rotation2d(Math.PI))
+                          : RobotState.getRobotPoseField().getRotation()));
+            },
+            drive)
+        .beforeStarting(
+            () -> {
+              distanceBefore[0] = drive.getModulePositions()[0].distanceMeters;
+            });
+  }
+
   public static final Command stop(Drive drive) {
     return Commands.run(() -> drive.stopWithX());
   }
