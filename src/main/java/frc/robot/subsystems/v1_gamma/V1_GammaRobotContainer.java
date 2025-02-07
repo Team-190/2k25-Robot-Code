0b58e43e6f4dcaf5071bf80eb2beec5beb0c7982
ElliotScher <ecscher84@gmail.com>
2025-02-06 23:20:40 -0500

    Add new LED number

--- src/main/java/frc/robot/subsystems/v1_gamma/V1_GammaRobotContainer.java
+++ src/main/java/frc/robot/subsystems/v1_gamma/V1_GammaRobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.v1_gamma;
 
+import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -16,20 +17,28 @@ import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.shared.drive.Drive;
 import frc.robot.subsystems.shared.drive.DriveConstants;
 import frc.robot.subsystems.shared.drive.GyroIO;
+import frc.robot.subsystems.shared.drive.GyroIOPigeon2;
 import frc.robot.subsystems.shared.drive.ModuleIO;
 import frc.robot.subsystems.shared.drive.ModuleIOSim;
+import frc.robot.subsystems.shared.drive.ModuleIOTalonFX;
 import frc.robot.subsystems.shared.vision.CameraConstants.RobotCameras;
 import frc.robot.subsystems.shared.vision.Vision;
 import frc.robot.subsystems.v1_gamma.elevator.V1_GammaElevator;
 import frc.robot.subsystems.v1_gamma.elevator.V1_GammaElevatorIO;
 import frc.robot.subsystems.v1_gamma.elevator.V1_GammaElevatorIOSim;
+import frc.robot.subsystems.v1_gamma.elevator.V1_GammaElevatorIOTalonFX;
 import frc.robot.subsystems.v1_gamma.funnel.V1_GammaFunnel;
 import frc.robot.subsystems.v1_gamma.funnel.V1_GammaFunnelIO;
 import frc.robot.subsystems.v1_gamma.funnel.V1_GammaFunnelIOSim;
+import frc.robot.subsystems.v1_gamma.funnel.V1_GammaFunnelIOTalonFX;
 import frc.robot.subsystems.v1_gamma.leds.V1_Gamma_LEDs;
 import frc.robot.subsystems.v1_gamma.manipulator.V1_GammaManipulator;
 import frc.robot.subsystems.v1_gamma.manipulator.V1_GammaManipulatorIO;
 import frc.robot.subsystems.v1_gamma.manipulator.V1_GammaManipulatorIOSim;
+import frc.robot.subsystems.v1_gamma.manipulator.V1_GammaManipulatorIOTalonFX;
+import frc.robot.util.LTNUpdater;
+
+import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class V1_GammaRobotContainer implements RobotContainer {
@@ -56,17 +65,17 @@ public class V1_GammaRobotContainer implements RobotContainer {
     if (Constants.getMode() != Mode.REPLAY) {
       switch (Constants.ROBOT) {
         case V1_GAMMA:
-          // drive =
-          //     new Drive(
-          //         new GyroIOPigeon2(),
-          //         new ModuleIOTalonFX(DriveConstants.FRONT_LEFT),
-          //         new ModuleIOTalonFX(DriveConstants.FRONT_RIGHT),
-          //         new ModuleIOTalonFX(DriveConstants.BACK_LEFT),
-          //         new ModuleIOTalonFX(DriveConstants.BACK_RIGHT));
-          // vision = new Vision(RobotCameras.v1_GammaCams);
-          // elevator = new V1_GammaElevator(new V1_GammaElevatorIOTalonFX());
-          // funnel = new V1_GammaFunnel(new V1_GammaFunnelIOTalonFX());
-          // manipulator = new V1_GammaManipulator(new V1_GammaManipulatorIOTalonFX());
+          drive =
+              new Drive(
+                  new GyroIOPigeon2(),
+                  new ModuleIOTalonFX(DriveConstants.FRONT_LEFT),
+                  new ModuleIOTalonFX(DriveConstants.FRONT_RIGHT),
+                  new ModuleIOTalonFX(DriveConstants.BACK_LEFT),
+                  new ModuleIOTalonFX(DriveConstants.BACK_RIGHT));
+          vision = new Vision(RobotCameras.v1_GammaCams);
+          elevator = new V1_GammaElevator(new V1_GammaElevatorIOTalonFX());
+          funnel = new V1_GammaFunnel(new V1_GammaFunnelIOTalonFX());
+          manipulator = new V1_GammaManipulator(new V1_GammaManipulatorIOTalonFX());
           leds = new V1_Gamma_LEDs();
           break;
         case V1_GAMMA_SIM:
@@ -176,23 +185,23 @@ public class V1_GammaRobotContainer implements RobotContainer {
 
   @Override
   public void robotPeriodic() {
-    // RobotState.periodic(
-    //     drive.getRawGyroRotation(),
-    //     NetworkTablesJNI.now(),
-    //     drive.getYawVelocity(),
-    //     drive.getFieldRelativeVelocity(),
-    //     drive.getModulePositions(),
-    //     vision.getCameras());
+    RobotState.periodic(
+        drive.getRawGyroRotation(),
+        NetworkTablesJNI.now(),
+        drive.getYawVelocity(),
+        drive.getFieldRelativeVelocity(),
+        drive.getModulePositions(),
+        vision.getCameras());
 
-    // LTNUpdater.updateDrive(drive);
-    // LTNUpdater.updateElevator(elevator);
-    // LTNUpdater.updateFunnel(funnel);
+    LTNUpdater.updateDrive(drive);
+    LTNUpdater.updateElevator(elevator);
+    LTNUpdater.updateFunnel(funnel);
 
-    // if (Constants.getMode().equals(Mode.SIM)) {
-    //   Logger.recordOutput(
-    //       "Component Poses",
-    //       V1_GammaMechanism3d.getPoses(elevator.getPosition(), funnel.getAngle()));
-    // }
+    if (Constants.getMode().equals(Mode.SIM)) {
+      Logger.recordOutput(
+          "Component Poses",
+          V1_GammaMechanism3d.getPoses(elevator.getPosition(), funnel.getAngle()));
+    }
   }
 
   @Override
