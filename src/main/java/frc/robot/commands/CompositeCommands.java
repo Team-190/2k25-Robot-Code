0703b8bcb82a8b7db48e5e87cf906e1e84992e07
Anshu Adiga <aadiga@wpi.edu>
2025-04-03 15:03:07 -0400

    Update at 'Thu Apr 03 15:03:07 EDT 2025'

--- src/main/java/frc/robot/commands/CompositeCommands.java
+++ src/main/java/frc/robot/commands/CompositeCommands.java
@@ -487,14 +487,13 @@ public class CompositeCommands {
 
       public static final Command netHeight(
           Elevator elevator, V2_RedundancyManipulator manipulator, V2_RedundancyIntake intake) {
-        return DecisionTree.moveSequence(
+        return Commands.sequence(DecisionTree.moveSequence(
                 elevator,
                 manipulator,
                 intake,
                 () -> ReefHeight.ALGAE_SCORE,
                 ArmState.PRE_SCORE,
-                IntakeState.STOW)
-            .until(() -> elevator.atGoal() && manipulator.algaeArmAtGoal() && intake.atGoal());
+                IntakeState.STOW), manipulator.setAlgaeArmGoal(ArmState.STOW_UP));
       }
 
       public static final Command autoScoreAlgae(
