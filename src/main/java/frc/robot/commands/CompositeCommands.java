a2b70ed9327df086fe2adce73e714449ffc2798d
Anshu Adiga <aadiga@wpi.edu>
2025-04-04 12:52:57 -0400

    Update at 'Fri Apr 04 12:52:56 EDT 2025'

--- src/main/java/frc/robot/commands/CompositeCommands.java
+++ src/main/java/frc/robot/commands/CompositeCommands.java
@@ -485,6 +485,34 @@ public class CompositeCommands {
             manipulator.scoreAlgae().withTimeout(0.5));
       }
 
+      public static final Command dropAlgae( Drive drive,
+        Elevator elevator, V2_RedundancyManipulator manipulator, V2_RedundancyIntake intake, Supplier<ReefHeight> level, Camera... cameras
+      ) {
+        return Commands.sequence(
+            DriveCommands.autoAlignReefAlgae(drive, cameras),
+            Commands.deadline(
+                Commands.sequence(
+                    DecisionTree.moveSequence(
+                        elevator, manipulator, intake, level, ArmState.STOW_DOWN, IntakeState.STOW),
+                    DecisionTree.moveSequence(
+                        elevator,
+                        manipulator,
+                        intake,
+                        level,
+                        ArmState.REEF_INTAKE,
+                        IntakeState.STOW),
+                    Commands.waitSeconds(1.0),
+                    Commands.runEnd(
+                            () -> drive.runVelocity(new ChassisSpeeds(1.0, 0.0, 0.0)),
+                            () -> drive.stop())
+                        .withTimeout(0.5)),
+                manipulator.intakeReefAlgae()),
+                manipulator.scoreAlgae().withTimeout(0.75),
+                DecisionTree.moveSequence(elevator, manipulator, intake, ()->ReefHeight.CORAL_INTAKE, ArmState.STOW_DOWN, IntakeState.STOW)
+
+        );
+      }
+
       public static final Command netHeight(
           Elevator elevator, V2_RedundancyManipulator manipulator, V2_RedundancyIntake intake) {
         return Commands.sequence(
