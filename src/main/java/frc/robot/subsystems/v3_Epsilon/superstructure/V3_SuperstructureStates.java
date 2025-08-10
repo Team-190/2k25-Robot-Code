3fb6ab60d6714f380ac6655891b2f336e6b62cdc
iamabhiveni <iamabhiveni@gmail.com>
2025-08-10 12:54:51 -0400

    Worked on V3_SuperstructureStates

--- src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureStates.java
+++ src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureStates.java
@@ -1,13 +1,20 @@
 package frc.robot.subsystems.v3_Epsilon.superstructure;
 
 // Adjust the package path as needed
+import frc.robot.FieldConstants;
+import frc.robot.subsystems.v3_Epsilon.intake.V3_EpsilonIntakeConstants;
+import frc.robot.subsystems.v3_Epsilon.manipulator.V3_EpsilonManipulatorConstants;
 import frc.robot.subsystems.v3_Epsilon.superstructure.V3_SuperstructureActions.SubsystemActions;
 import frc.robot.subsystems.v3_Epsilon.superstructure.V3_SuperstructurePoses.SubsystemPoses; // Ensure this is the correct package path
 
 public enum V3_SuperstructureStates {
   START("START", new SubsystemPoses(), SubsystemActions.empty()),
   STOW_DOWN("STOW_DOWN", new SubsystemPoses(), SubsystemActions.empty()),
-  STOW_UP("STOW_UP", new SubsystemPoses(), SubsystemActions.empty());
+  STOW_UP("STOW_UP", new SubsystemPoses(
+    FieldConstants.Reef.ReefState.STOW, 
+    V3_EpsilonManipulatorConstants.PivotState.STOW_UP, 
+    V3_EpsilonIntakeConstants.IntakeState.STOW
+  ), SubsystemActions.empty());
 
   private final String name;
   private final SubsystemPoses pose;
