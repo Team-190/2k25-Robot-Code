74d6d612b9108b94be4521290018ce7c4de5206c
iamabhiveni <iamabhiveni@gmail.com>
2025-08-10 22:50:21 -0400

    Will work on more states for V3 later. This is a good baseline for now

--- src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureStates.java
+++ src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureStates.java
@@ -80,7 +80,96 @@ public enum V3_SuperstructureStates {
         V3_EpsilonIntakeConstants.IntakeState.STOW),
     new SubsystemActions(
         V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_CORAL,
-        V3_EpsilonIntakeConstants.IntakeRollerStates.STOP));
+        V3_EpsilonIntakeConstants.IntakeRollerStates.STOP)),
+
+  L4_SCORE(
+    "L4_SCORE",
+    new SubsystemPoses(
+        FieldConstants.Reef.ReefState.L4,
+        V3_EpsilonManipulatorConstants.PivotState.PRE_SCORE, // Determine whether PRE_SCORE is accurate for all scoring states
+        V3_EpsilonIntakeConstants.IntakeState.STOW),
+    new SubsystemActions(
+        V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_CORAL,
+        V3_EpsilonIntakeConstants.IntakeRollerStates.STOP)
+  ),
+
+  // Algae Prep States
+  L2_ALGAE_PREP(
+        "L2_ALGAE_PREP",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.L2,
+            V3_EpsilonManipulatorConstants.PivotState.PRE_SCORE,
+            V3_EpsilonIntakeConstants.IntakeState.STOW),
+        SubsystemActions.empty()),
+
+  L3_ALGAE_PREP(
+        "L3_ALGAE_PREP",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.L3,
+            V3_EpsilonManipulatorConstants.PivotState.PRE_SCORE,
+            V3_EpsilonIntakeConstants.IntakeState.STOW),
+        SubsystemActions.empty()),
+
+  // Algae Scoring States
+  L2_ALGAE_SCORE(
+        "L2_ALGAE_SCORE",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.L2,
+            V3_EpsilonManipulatorConstants.PivotState.PRE_SCORE,
+            V3_EpsilonIntakeConstants.IntakeState.STOW),
+        new SubsystemActions(
+            V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_ALGAE,
+            V3_EpsilonIntakeConstants.IntakeRollerStates.STOP)),
+  
+  L3_ALGAE_SCORE(
+        "L3_ALGAE_SCORE",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.L3,
+            V3_EpsilonManipulatorConstants.PivotState.PRE_SCORE,
+            V3_EpsilonIntakeConstants.IntakeState.STOW),
+        new SubsystemActions(
+            V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_ALGAE,
+            V3_EpsilonIntakeConstants.IntakeRollerStates.STOP)),
+
+  // Miscelaneous States (Barge + Processor)
+  BARGE_PREP(
+        "BARGE_PREP",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.ASS_TOP,
+            V3_EpsilonManipulatorConstants.PivotState.REEF_INTAKE, // Assuming REEF_INTAKE and BARGE_PREP have same rotation values
+            V3_EpsilonIntakeConstants.IntakeState.STOW),
+        SubsystemActions.empty()),
+
+  PROCESSOR_PREP(
+    "PROCESSOR_PREP",
+    new SubsystemPoses(
+        FieldConstants.Reef.ReefState.ASS_BOT,
+        V3_EpsilonManipulatorConstants.PivotState.PROCESSOR,
+        V3_EpsilonIntakeConstants.IntakeState.STOW),
+    SubsystemActions.empty()),
+
+  BARGE_SCORE(
+    "BARGE_SCORE",
+    new SubsystemPoses(
+        FieldConstants.Reef.ReefState.ASS_TOP, // Assuming ASS_TOP is the max height the elevator can reach. Check later
+        V3_EpsilonManipulatorConstants.PivotState.REEF_INTAKE,
+        V3_EpsilonIntakeConstants.IntakeState.STOW),
+    new SubsystemActions(
+        V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_ALGAE, // Assuming you score in the barge by stowing intake and moving manipulator to desired position and then scoring
+        V3_EpsilonIntakeConstants.IntakeRollerStates.STOP
+    )),
+
+   PROCESSOR_SCORE(
+        "PROCESSOR_SCORE",
+        new SubsystemPoses(
+            FieldConstants.Reef.ReefState.ASS_BOT,
+            V3_EpsilonManipulatorConstants.PivotState.PROCESSOR,
+            V3_EpsilonIntakeConstants.IntakeState.STOW
+        ),
+        new SubsystemActions(
+            V3_EpsilonManipulatorConstants.ManipulatorRollerStates.SCORE_ALGAE,
+            V3_EpsilonIntakeConstants.IntakeRollerStates.STOP
+        ));
 
   private final String name;
   private final SubsystemPoses pose;
