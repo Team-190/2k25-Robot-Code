1dcb7fa8ae78ae60cd5f5c40ec43cee2e210ea6b
iamabhiveni <iamabhiveni@gmail.com>
2025-08-12 12:54:36 -0400

    Commented code because of build errors

--- src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureEdges.java
+++ src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/V3_SuperstructureEdges.java
@@ -1,14 +1,9 @@
 package frc.robot.subsystems.v3_Epsilon.superstructure;
+
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import lombok.Builder;
-import lombok.Getter;
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultEdge;
 
 public class V3_SuperstructureEdges {
-  
+
   public static final ArrayList<Edge> AlgaeEdges = new ArrayList<>();
   public static final ArrayList<Edge> CoralEdges = new ArrayList<>();
 
@@ -18,32 +13,25 @@ public class V3_SuperstructureEdges {
     }
 
     public String toString() {
-      return String.format(
-          "Edge[from=%s, to=%s, action=%s]", from.name(), to.name(), action);
+      return String.format("Edge[from=%s, to=%s, action=%s]", from.name(), to.name(), action);
     }
   }
-  
+
   public enum AlgaeEdge {
     NONE,
     NO_ALGAE,
     ALGAE
   }
 
-  @Builder(toBuilder = true)
-  @Getter
-  public static class EdgeCommand extends DefaultEdge {
-    private final Command command;
-    @Builder.Default private final AlgaeEdge algaeEdgeType = AlgaeEdge.NONE;
-  }
+  // @Builder(toBuilder = true)
+  // @Getter
+  // public static class EdgeCommand extends DefaultEdge {
+  //   private final Command command;
+  //   @Builder.Default private final AlgaeEdge algaeEdgeType = AlgaeEdge.NONE;
+  // }
 
-  private static Command getEdgeCommand(
-      V3_SuperstructureStates from, V3_SuperstructureStates to, AlgaeEdge algaeEdgeType) {
-    return Command.builder()
-        .from(from)
-        .to(to)
-        .algaeEdgeType(algaeEdgeType)
-        .build();
-  }
-
-  
+  // private static Command getEdgeCommand(
+  //     V3_SuperstructureStates from, V3_SuperstructureStates to, AlgaeEdge algaeEdgeType) {
+  //   return Command.builder().from(from).to(to).algaeEdgeType(algaeEdgeType).build();
+  // }
 }
