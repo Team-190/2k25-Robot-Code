28c2d3d24098f6e432ce5dbda2ce8f306f9a7320
vaddadisri26 <vaddadi.sriaditya@gmail.com>
2025-09-25 18:54:09 -0400

    Pre-bringup code cleanup; next step is to test transitions in sim.
    Co-authored-by: SeptimusHeap7 <SeptimusHeap7@users.noreply.github.com>
    Co-authored-by: Anshu <aadiga@wpi.edu>
    Co-authored-by: jasminepalit <jasminepalit@users.noreply.github.com>
    Co-authored-by: Abhiraam Venigalla <Abhi52897340598327@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/v3_Epsilon/V3_EpsilonRobotContainer.java
+++ src/main/java/frc/robot/subsystems/v3_Epsilon/V3_EpsilonRobotContainer.java
@@ -2,19 +2,14 @@ package frc.robot.subsystems.v3_Epsilon;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTablesJNI;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import frc.robot.Constants.Mode;
 import frc.robot.RobotContainer;
 import frc.robot.RobotState;
-import frc.robot.commands.CompositeCommands;
 import frc.robot.commands.CompositeCommands.V3_EpsilonCompositeCommands;
-import frc.robot.commands.CompositeCommands;
 import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.shared.drive.Drive;
 import frc.robot.subsystems.shared.drive.DriveConstants;
@@ -68,68 +63,63 @@ public class V3_EpsilonRobotContainer implements RobotContainer {
     if (Constants.getMode() != Mode.REPLAY) {
       switch (Constants.ROBOT) {
         case V3_EPSILON:
-          drive = new Drive(
-              new GyroIOPigeon2(),
-              new ModuleIOTalonFX(0, DriveConstants.FRONT_LEFT),
-              new ModuleIOTalonFX(1, DriveConstants.FRONT_RIGHT),
-              new ModuleIOTalonFX(2, DriveConstants.BACK_LEFT),
-              new ModuleIOTalonFX(3, DriveConstants.BACK_RIGHT));
+          drive =
+              new Drive(
+                  new GyroIOPigeon2(),
+                  new ModuleIOTalonFX(0, DriveConstants.FRONT_LEFT),
+                  new ModuleIOTalonFX(1, DriveConstants.FRONT_RIGHT),
+                  new ModuleIOTalonFX(2, DriveConstants.BACK_LEFT),
+                  new ModuleIOTalonFX(3, DriveConstants.BACK_RIGHT));
           elevator = new Elevator(new ElevatorIOTalonFX()).getFSM();
           intake = new V3_EpsilonIntake(new V3_EpsilonIntakeIOTalonFX());
           manipulator = new V3_EpsilonManipulator(new V3_EpsilonManipulatorIOTalonFX());
           climber = new V3_EpsilonClimber(new V3_EpsilonClimberIOTalonFX());
           superstructure = new V3_EpsilonSuperstructure(elevator, intake, manipulator);
-          vision = new Vision(
-              () -> AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded),
-              RobotCameras.V2_REDUNDANCY_CAMS);
+          vision =
+              new Vision(
+                  () -> AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded),
+                  RobotCameras.V2_REDUNDANCY_CAMS);
           break;
         case V3_EPSILON_SIM:
-          drive = new Drive(
-              new GyroIO() {
-              },
-              new ModuleIOSim(DriveConstants.FRONT_LEFT),
-              new ModuleIOSim(DriveConstants.FRONT_RIGHT),
-              new ModuleIOSim(DriveConstants.BACK_LEFT),
-              new ModuleIOSim(DriveConstants.BACK_RIGHT));
+          drive =
+              new Drive(
+                  new GyroIO() {},
+                  new ModuleIOSim(DriveConstants.FRONT_LEFT),
+                  new ModuleIOSim(DriveConstants.FRONT_RIGHT),
+                  new ModuleIOSim(DriveConstants.BACK_LEFT),
+                  new ModuleIOSim(DriveConstants.BACK_RIGHT));
           elevator = new Elevator(new ElevatorIOSim()).getFSM();
           intake = new V3_EpsilonIntake(new V3_EpsilonIntakeIOSim());
           manipulator = new V3_EpsilonManipulator(new V3_EpsilonManipulatorIOSim());
           climber = new V3_EpsilonClimber(new V3_EpsilonClimberIOSim());
           superstructure = new V3_EpsilonSuperstructure(elevator, intake, manipulator);
-          vision = new Vision(() -> AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded));
+          vision =
+              new Vision(() -> AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded));
           break;
         default:
           break;
       }
 
       if (drive == null) {
-        drive = new Drive(
-            new GyroIO() {
-            },
-            new ModuleIO() {
-            },
-            new ModuleIO() {
-            },
-            new ModuleIO() {
-            },
-            new ModuleIO() {
-            });
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
       }
       if (elevator == null) {
-        elevator = new Elevator(new ElevatorIO() {
-        }).getFSM();
+        elevator = new Elevator(new ElevatorIO() {}).getFSM();
       }
       if (intake == null) {
-        intake = new V3_EpsilonIntake(new V3_EpsilonIntakeIO() {
-        });
+        intake = new V3_EpsilonIntake(new V3_EpsilonIntakeIO() {});
       }
       if (manipulator == null) {
-        manipulator = new V3_EpsilonManipulator(new V3_EpsilonManipulatorIO() {
-        });
+        manipulator = new V3_EpsilonManipulator(new V3_EpsilonManipulatorIO() {});
       }
       if (climber == null) {
-        climber = new V3_EpsilonClimber(new V3_EpsilonClimberIO() {
-        });
+        climber = new V3_EpsilonClimber(new V3_EpsilonClimberIO() {});
       }
       if (superstructure == null) {
         superstructure = new V3_EpsilonSuperstructure(elevator, intake, manipulator);
@@ -139,9 +129,8 @@ public class V3_EpsilonRobotContainer implements RobotContainer {
   }
 
   /**
-   * Configure the button bindings for the robot. This method is called in
-   * the constructor and is responsible for setting up the default commands
-   * for each button on the controllers.
+   * Configure the button bindings for the robot. This method is called in the constructor and is
+   * responsible for setting up the default commands for each button on the controllers.
    */
   private void configureButtonBindings() {
     drive.setDefaultCommand(
@@ -161,13 +150,11 @@ public class V3_EpsilonRobotContainer implements RobotContainer {
         .whileFalse(superstructure.runGoal(V3_EpsilonSuperstructureStates.HANDOFF));
   }
 
-  private void configureAutos() {
-  }
+  private void configureAutos() {}
 
   /**
-   * Periodic function for the robot. This function is called every 20ms,
-   * and is responsible for updating the robot's state and logging relevant
-   * data.
+   * Periodic function for the robot. This function is called every 20ms, and is responsible for
+   * updating the robot's state and logging relevant data.
    */
   @Override
   public void robotPeriodic() {
@@ -193,25 +180,13 @@ public class V3_EpsilonRobotContainer implements RobotContainer {
   }
 
   /**
-   * Returns the autonomous command for the robot. This command will be scheduled
-   * for the entire autonomous period.
+   * Returns the autonomous command for the robot. This command will be scheduled for the entire
+   * autonomous period.
    *
    * @return the autonomous command for the robot
    */
   @Override
   public Command getAutonomousCommand() {
-    return Commands.runOnce(
-        () -> RobotState.resetRobotPose(new Pose2d(7.25, 2.39, new Rotation2d(Math.PI / 2))))
-        .andThen(CompositeCommands.V3_EpsilonCompositeCommands.optimalScoreBarge(superstructure));
-    // return superstructure.allTransition();
-    // return Commands.sequence(
-    // V3_EpsilonCompositeCommands.dropAlgae(
-    // drive,
-    // elevator,
-    // manipulator,
-    // intake,
-    // superstructure,
-    // () -> ReefState.ALGAE_INTAKE_TOP,
-    // RobotCameras.V3_EPSILON_CAMS));
+    return superstructure.allTransition();
   }
 }
--- src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/intake/V3_EpsilonIntakeConstants.java
+++ src/main/java/frc/robot/subsystems/v3_Epsilon/superstructure/intake/V3_EpsilonIntakeConstants.java
@@ -2,64 +2,72 @@ package frc.robot.subsystems.v3_Epsilon.superstructure.intake;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
+import frc.robot.Constants;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 public class V3_EpsilonIntakeConstants {
+
   public static final int PIVOT_CAN_ID;
-
-  public static final int ROLLER_CAN_ID_INNER;
   public static final int ROLLER_CAN_ID_OUTER;
-
+  public static final int ROLLER_CAN_ID_INNER;
   public static final int LEFT_SENSOR_CAN_ID;
-
   public static final int RIGHT_SENSOR_CAN_ID;
 
   public static final double INTAKE_CAN_CORAL_DETECTED_THRESHOLD_METERS;
 
-  public static final IntakeCurrentLimits CURRENT_LIMITS =
-      new IntakeCurrentLimits(40.0, 40.0, 40.0, 40.0);
+  public static final IntakeCurrentLimits CURRENT_LIMITS;
 
-  public static final Gains PIVOT_GAINS = new Gains(1.0, 0.01, 0.0, 0.0, 0.0, 0.0);
-  public static final Constraints PIVOT_CONSTRAINTS =
-      new Constraints(500.0, 500.0, Rotation2d.fromDegrees(1.5));
+  public static final Gains PIVOT_GAINS;
+  public static final Constraints PIVOT_CONSTRAINTS;
 
-  public static final IntakeParems PIVOT_PARAMS =
-      new IntakeParems(
-          3.0,
-          DCMotor.getKrakenX60Foc(1),
-          0.0042,
-          Rotation2d.fromDegrees(0.0),
-          Rotation2d.fromDegrees(124.6));
-  public static final IntakeParems ROLLER_PARAMS =
-      new IntakeParems(
-          1, DCMotor.getKrakenX60Foc(1), 0, new Rotation2d(), Rotation2d.fromDegrees(0));
+  public static final IntakeParems PIVOT_PARAMS;
+  public static final IntakeParems ROLLER_PARAMS;
 
   static {
     PIVOT_CAN_ID = 60;
-    ROLLER_CAN_ID_OUTER =
-        61; // This used to be 61, but there are two motors, so I replace this with 42 until it gets
-    // sorted out fs
-    ROLLER_CAN_ID_INNER = 62; // This one I just created.
-  }
-
-  static {
+    ROLLER_CAN_ID_OUTER = 61; // TODO: Check numbers here
+    ROLLER_CAN_ID_INNER = 62; // TODO: Check numbers here
     LEFT_SENSOR_CAN_ID = 0; // TODO: Check numbers here
     RIGHT_SENSOR_CAN_ID = 1;
-  }
 
-  static {
-    INTAKE_CAN_CORAL_DETECTED_THRESHOLD_METERS =
-        42; // In meters= 42; TODO: Set value after robot built
+    INTAKE_CAN_CORAL_DETECTED_THRESHOLD_METERS = 0.05; // TODO: Check this value
+
+    PIVOT_PARAMS =
+        new IntakeParems(
+            3.0,
+            DCMotor.getKrakenX60Foc(1),
+            0.0042,
+            Rotation2d.fromDegrees(0.0),
+            Rotation2d.fromDegrees(124.6));
+    ROLLER_PARAMS =
+        new IntakeParems(
+            1, DCMotor.getKrakenX60Foc(1), 0, new Rotation2d(), Rotation2d.fromDegrees(0));
+
+    switch (Constants.ROBOT) {
+      case V3_EPSILON_SIM:
+        PIVOT_CONSTRAINTS = new Constraints(500.0, 500.0, Rotation2d.fromDegrees(1.5));
+        PIVOT_GAINS = new Gains(0.0, 0.00, 0.0, 0.0, 0.0, 0.0);
+        CURRENT_LIMITS = new IntakeCurrentLimits(40.0, 40.0, 40.0, 40.0, 40.0, 40.0);
+
+        break;
+
+      default:
+        PIVOT_CONSTRAINTS = new Constraints(500.0, 500.0, Rotation2d.fromDegrees(1.5));
+        PIVOT_GAINS = new Gains(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+        CURRENT_LIMITS = new IntakeCurrentLimits(40.0, 40.0, 40.0, 40.0, 40.0, 40.0);
+
+        break;
+    }
   }
 
   @RequiredArgsConstructor
   public enum IntakePivotState {
-    STOW(Rotation2d.fromDegrees(25.0)),
-    INTAKE_CORAL(Rotation2d.fromDegrees(123.6)),
-    HANDOFF(Rotation2d.fromDegrees(0)),
-    L1(Rotation2d.fromDegrees(-82 + 123.6)),
-    INTAKE_ALGAE(new Rotation2d()),
+    STOW(Rotation2d.fromDegrees(-123.6)),
+    INTAKE_CORAL(Rotation2d.fromDegrees(0)),
+    HANDOFF(Rotation2d.fromDegrees(-123.6)),
+    L1(Rotation2d.fromDegrees(-82)),
+    INTAKE_ALGAE(new Rotation2d(0)),
     ARM_CLEAR(Rotation2d.fromDegrees(35));
 
     @Getter private final Rotation2d angle;
@@ -68,8 +76,10 @@ public class V3_EpsilonIntakeConstants {
   public static record IntakeCurrentLimits(
       double PIVOT_SUPPLY_CURRENT_LIMIT,
       double PIVOT_STATOR_CURRENT_LIMIT,
-      double ROLLER_SUPPLY_CURRENT_LIMIT,
-      double ROLLER_STATOR_CURRENT_LIMIT) {}
+      double INNER_ROLLER_SUPPLY_CURRENT_LIMIT,
+      double INNER_ROLLER_STATOR_CURRENT_LIMIT,
+      double OUTER_ROLLER_SUPPLY_CURRENT_LIMIT,
+      double OUTER_ROLLER_STATOR_CURRENT_LIMIT) {}
 
   public static record Gains(double kP, double kD, double kS, double kV, double kA, double kG) {}
 
