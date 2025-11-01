00c0672ec6d259df93f68426d2883bf51874a032
Anshu Adiga <aadiga@wpi.edu>
2025-11-01 12:29:57 -0400

    climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,58 +3,58 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Constants {
-    public static final boolean TUNING_MODE = false;
-    public static final double LOOP_PERIOD_SECONDS = 0.02;
-    public static final RobotType ROBOT = RobotType.V3_EPSILON;
+  public static final boolean TUNING_MODE = false;
+  public static final double LOOP_PERIOD_SECONDS = 0.02;
+  public static final RobotType ROBOT = RobotType.V3_EPSILON;
 
-    public static Mode getMode() {
-        switch (ROBOT) {
-            case V0_FUNKY:
-            case V0_WHIPLASH:
-            case V0_GOMPEIVISION_TEST:
-            case V1_STACKUP:
-            case V2_REDUNDANCY:
-            case V3_EPSILON:
-                return RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
+  public static Mode getMode() {
+    switch (ROBOT) {
+      case V0_FUNKY:
+      case V0_WHIPLASH:
+      case V0_GOMPEIVISION_TEST:
+      case V1_STACKUP:
+      case V2_REDUNDANCY:
+      case V3_EPSILON:
+        return RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
 
-            case V0_FUNKY_SIM:
-            case V0_WHIPLASH_SIM:
-            case V0_GOMPEIVISION_TEST_SIM:
-            case V1_STACKUP_SIM:
-            case V2_REDUNDANCY_SIM:
-            case V3_EPSILON_SIM:
-                return Mode.SIM;
+      case V0_FUNKY_SIM:
+      case V0_WHIPLASH_SIM:
+      case V0_GOMPEIVISION_TEST_SIM:
+      case V1_STACKUP_SIM:
+      case V2_REDUNDANCY_SIM:
+      case V3_EPSILON_SIM:
+        return Mode.SIM;
 
-            default:
-                return Mode.REAL;
-        }
+      default:
+        return Mode.REAL;
     }
+  }
 
-    public static enum Mode {
-        REAL,
-        SIM,
-        REPLAY
-    }
+  public static enum Mode {
+    REAL,
+    SIM,
+    REPLAY
+  }
 
-    public static enum RobotType {
-        V0_FUNKY,
-        V0_FUNKY_SIM,
-        V0_WHIPLASH,
-        V0_WHIPLASH_SIM,
-        V0_GOMPEIVISION_TEST,
-        V0_GOMPEIVISION_TEST_SIM,
-        V1_STACKUP,
-        V1_STACKUP_SIM,
-        V2_REDUNDANCY,
-        V2_REDUNDANCY_SIM,
-        V3_EPSILON_SIM,
-        V3_EPSILON,
-    }
+  public static enum RobotType {
+    V0_FUNKY,
+    V0_FUNKY_SIM,
+    V0_WHIPLASH,
+    V0_WHIPLASH_SIM,
+    V0_GOMPEIVISION_TEST,
+    V0_GOMPEIVISION_TEST_SIM,
+    V1_STACKUP,
+    V1_STACKUP_SIM,
+    V2_REDUNDANCY,
+    V2_REDUNDANCY_SIM,
+    V3_EPSILON_SIM,
+    V3_EPSILON,
+  }
 
-    public static void main(String... args) {
-        if (ROBOT == RobotType.V1_STACKUP_SIM) {
-            System.err.println("Cannot deploy, invalid mode selected: " + ROBOT.toString());
-            System.exit(1);
-        }
+  public static void main(String... args) {
+    if (ROBOT == RobotType.V1_STACKUP_SIM) {
+      System.err.println("Cannot deploy, invalid mode selected: " + ROBOT.toString());
+      System.exit(1);
     }
+  }
 }
